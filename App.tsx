d50d40086ea9a7b968a4ec60b691cbd7c03c4e57
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-01-26 08:37:18 -0300

    Folder Structuregit

--- App.tsx
+++ App.tsx
@@ -32,26 +32,9 @@ type SectionProps = PropsWithChildren<{
 function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <SafeAreaView>
+      <Text>Texto Teste</Text>
+    </SafeAreaView>
   );
 }
 
